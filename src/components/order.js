7523a5adb8414eab65bf9220f7099faa6f386863
Ryan Lewis <rlewis94@gmail.com>
2021-09-29 19:41:55 -0700

    set up mui cards

--- src/components/order.js
+++ src/components/order.js
@@ -1,39 +1,78 @@
 import React, { Component } from 'react';
 import {
-  Card,
-  CardImg,
-  CardText,
-  CardBody,
-  CardTitle,
+  // Card,
+  // CardImg,
+  // CardText,
+  // CardBody,
+  // CardTitle,
   // CardSubtitle,
   Container,
   Row,
   Col
 } from 'reactstrap';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
 
-function makeCards(arr){ if(arr){
-    return( arr.map((d) => {
-      return(
-        <Col lg="3" md="6" className="cardCol">
-          <Card>
-            <CardBody>
-              <CardTitle tag="h3">{d.item_data.name}</CardTitle>
-              {/* The card images might have problems if there's multiple pics */}
-              <CardImg width="100%" src={d.item_data.ecom_image_uris}/>
-              <div>
-                <CardText>
-                  {d.item_data.description}
-                </CardText>
-              </div>
-              <div className="grow" />
-              <div className="priceBox">
-                <b>Price:</b> ${(d.item_data.variations[0].item_variation_data.price_money.amount/100).toFixed(2)}
-              </div>
-            </CardBody>
-          </Card>
-        </Col>
-      );
-    })
+// function makeCardsBootStrap(arr){ if(arr){
+//     return( arr.map((d) => {
+//       return(
+//         <Col lg="3" md="6" className="cardCol">
+//           <Card>
+//             <CardBody>
+//               <CardTitle tag="h3">{d.item_data.name}</CardTitle>
+//               {/* The card images might have problems if there's multiple pics */}
+//               <CardImg width="100%" src={d.item_data.ecom_image_uris}/>
+//               <div>
+//                 <CardText>
+//                   {d.item_data.description}
+//                 </CardText>
+//               </div>
+//               <div className="grow" />
+//               <div className="priceBox">
+//                 <b>Price:</b> ${(d.item_data.variations[0].item_variation_data.price_money.amount/100).toFixed(2)}
+//               </div>
+//             </CardBody>
+//           </Card>
+//         </Col>
+//       );
+//     })
+//   )
+// }};
+
+function makeCardsMUI(arr){ if(arr){
+  return( arr.map((d) => {
+    return(
+      <Col lg="3" md="6" className="cardCol">
+        <Card sx={{ maxWidth: 345 }} className="card">
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image={d.item_data.ecom_image_uris}
+              // alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {d.item_data.name}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {d.item_data.description}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <b>Price:</b> ${(d.item_data.variations[0].item_variation_data.price_money.amount/100).toFixed(2)}
+            <Button size="small" color="primary">
+              Add to cart
+            </Button>
+          </CardActions>
+        </Card>
+      </Col>
+    );
+  })
   )
 }};
 
@@ -68,7 +107,8 @@ class Order extends Component {
     return (
       <Container className="main">
         <Row>
-          {makeCards(this.state.data)}
+          {/* {makeCardsBootStrap(this.state.data)} */}
+          {makeCardsMUI(this.state.data)}
         </Row>
       </Container>
     );
